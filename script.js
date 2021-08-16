672342316a56ca2c85128072e6a88f142b48e9ab
Ren Zheng <88332135+RENZ1104@users.noreply.github.com>
2021-08-16 20:12:31 +0000

    Final Version 2

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ x.style.width = "425px";
 }
 
 function normalImgInstagram(x) {
-x.style.height = "300px";
-x.style.width = "300px";
+x.style.height = "280px";
+x.style.width = "280px";
 }
 
 function bigImgWechat(x) {
@@ -26,5 +26,15 @@ x.style.width = "375px";
 function normalImgWechat(x) {
 x.style.height = "250px";
 x.style.width = "250px";
+}
+
+function bigImgLogo(x){
+x.style.height = "300px";
+x.style.width = "300px";
+}
+
+function normalImgLogo(x){
+x.style.height = "230px";
+x.style.width = "230px";
 }
 
